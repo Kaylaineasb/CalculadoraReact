20e8f0eb2cc29aba6d2ff08db92295d961932e9d
Kaylaine Assunção <kaylaineasb9@gmail.com>
2024-08-24 13:49:24 -0300

    first commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,8 @@
+import { ButtonContainer } from "./style";
+function Button({label,onClick}) {
+  return (
+    <ButtonContainer onClick={onClick}>{label}</ButtonContainer>
+  )
+}
+
+export default Button;
---
+++ src/components/Input/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width:100%;
+    height:75px;
+    background-color:#AAAAFF;
+
+    display:flex;
+    align-items:center;
+
+    input{
+        width:100%;
+        height:75px;
+        background-color:#AAAAFF;
+        border:0;
+        font-size:24px;
+        padding:0 10px;
+        color:#FFFFFF;
+        text-align:right;
+    }
+`
